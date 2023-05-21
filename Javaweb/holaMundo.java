eca7c41509e8b3ea99456d0593dc35bc08137bdd
Adrian Calderon <ascalderon21@gmail.com>
2023-05-20 23:27:08 -0500

    otra prueba

---
+++ Javaweb/holaMundo.java
@@ -0,0 +1,9 @@
+package Javaweb;
+
+public class holaMundo {
+    public static void main(String[] args) {
+        String  cadena = "Hola mundo";
+        System.out.println("saludando desde Java : " + cadena);
+    }
+    
+}
