f1a4f0da6c79b9b73160bdaa7bea69629e8e3c35
Adrian Calderon <ascalderon21@gmail.com>
2023-05-20 23:22:29 -0500

    Subiendo archivos de prueba

---
+++ miSaludo.java
@@ -0,0 +1,11 @@
+public class miSaludo{
+public static void main(String[] args) {
+
+    int x = 15;
+    int y = 20;
+    int r = x*y;
+
+    System.out.println("El resultado es:" +r);     
+}
+    
+}
